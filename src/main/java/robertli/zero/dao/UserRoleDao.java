3a0331b71dec8cae950f78e4a4d1071740bfb4f8
robert <li.liufv@gmail.com>
2017-10-16 18:22:14 -0700

    first commit

---
+++ src/main/java/robertli/zero/dao/UserRoleDao.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016 Robert Li.
+ * Released under the MIT license
+ * https://opensource.org/licenses/MIT
+ */
+package robertli.zero.dao;
+
+import robertli.zero.entity.UserRole;
+
+/**
+ *
+ * @author Robert Li
+ */
+public interface UserRoleDao extends GenericDao<UserRole, String> {
+
+    public void addUserRole(String name);
+}
